da558decce4f4f788588e7373c0a946c05fdc52e
Lafablee <enguerraneckert@ik.me>
2025-01-28 18:53:22 +0100

    Ajout fonction REDIS, histrory First Test

--- config.py
+++ config.py
@@ -15,3 +15,6 @@ redis_client = redis.Redis(
 
     decode_responses=False
 )
+
+TEMP_STORAGE_DURATION = 24 * 60 * 60  # 24 heures en secondes
+PERMANENT_STORAGE_DURATION = 30 * 24 * 60 * 60  # 30 jours en secondes
